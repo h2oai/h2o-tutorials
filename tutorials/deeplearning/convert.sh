626f19be3a6f461cc80f8af534a644ccdbcd1205
Arno Candel <arno.candel@gmail.com>
2015-11-06 12:34:22 -0800

    Add README.md (direct copy of deeplearning.md)

--- tutorials/deeplearning/convert.sh
+++ tutorials/deeplearning/convert.sh
@@ -2,6 +2,7 @@
 
 ## Turn R Markdown into regular Markdown
 sed -e 's/```{r.*}/```/' deeplearning.Rmd > deeplearning.md
+cp deeplearning.md README.md
 
 ## Turn R Markdown into plain R
 sed -e '1,\%```{r.*}%s:^:#:;/^```/,\%```{r.*}%s:^:#:;/```/d' deeplearning.Rmd > deeplearning.R
