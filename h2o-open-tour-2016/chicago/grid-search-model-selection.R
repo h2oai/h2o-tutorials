115eea2d3ef2c7e18700ff5e45ff44a2b1f4b433
ledell <erin@h2o.ai>
2016-05-03 00:44:51 -0700

    Updates to Open H2O Chicago grid search in R

--- h2o-open-tour-2016/chicago/grid-search-model-selection.R
+++ h2o-open-tour-2016/chicago/grid-search-model-selection.R
@@ -17,28 +17,47 @@ h2o.init(nthreads = -1, #Number of threads -1 means use all cores on your machin
 # The response column, bad_loan, is 1 if the loan was bad, and 0 otherwise
 
 # Import the data
-loan_csv <- "https://raw.githubusercontent.com/h2oai/app-consumer-loan/master/data/loan.csv"
+loan_csv <- "/Users/me/h2oai/code/demos/lending_club/loan.csv"  # modify this for your machine
+# Alternatively, you can import the data directly from a URL
+#loan_csv <- "https://raw.githubusercontent.com/h2oai/app-consumer-loan/master/data/loan.csv"
 data <- h2o.importFile(loan_csv)  # 163,994 rows x 15 columns
+dim(data)
+# [1] 163994     15
 
 # Since we want to train a binary classification model, 
 # we must ensure that the response is coded as a factor
-# If the repsonse is 0/1, H2O will assume it's numeric,
+# If the response is 0/1, H2O will assume it's numeric,
 # which means that H2O will train a regression model instead
 data$bad_loan <- as.factor(data$bad_loan)  #encode the binary repsonse as a factor
-
+h2o.levels(data$bad_loan)  #optoional: after encoding, this shows the two factor levels, '0' and '1'
+# [1] "0" "1"
 
 # Partition the data into training, validation and test sets
-splits <- h2o.splitFrame(data, 
+splits <- h2o.splitFrame(data = data, 
                          ratios = c(0.7, 0.15),  #partition data into 70%, 15%, 15% chunks
                          seed = 1)  #setting a seed will guarantee reproducibility
 train <- splits[[1]]
-valid <- splits[[3]]
-test <- splits[[2]]
+valid <- splits[[2]]
+test <- splits[[3]]
+
+# Take a look at the size of each partition
+# Notice that h2o.splitFrame uses approximate splitting not exact splitting (for efficiency)
+# so these are not exactly 70%, 15% and 15% of the total rows
+nrow(train)  # 114914
+nrow(valid) # 24499
+nrow(test)  # 24581
 
 # Identify response and predictor variables
 y <- "bad_loan"
 x <- setdiff(names(data), c(y, "int_rate"))  #remove the interest rate column because it's correlated with the outcome
-
+print(x)
+# [1] "loan_amnt"             "term"                 
+# [3] "emp_length"            "home_ownership"       
+# [5] "annual_inc"            "verification_status"  
+# [7] "purpose"               "addr_state"           
+# [9] "dti"                   "delinq_2yrs"          
+# [11] "revol_util"            "total_acc"            
+# [13] "longest_credit_length"
 
 
 # Now that we have prepared the data, we can train some models
@@ -71,6 +90,34 @@ gbm_gridperf1 <- h2o.getGrid(grid_id = "gbm_grid1",
                              sort_by = "auc", 
                              decreasing = TRUE)
 print(gbm_gridperf1)
+# H2O Grid Details
+# ================
+#   
+# Grid ID: gbm_grid1 
+# Used hyper parameters: 
+# - sample_rate 
+# -  max_depth 
+# -  learn_rate 
+# -  col_sample_rate 
+# Number of models: 36 
+# Number of failed models: 0 
+# 
+# Hyper-Parameter Search Summary: ordered by decreasing auc
+# sample_rate max_depth learn_rate col_sample_rate          model_ids               auc
+# 1           1         3        0.1             0.5 gbm_grid1_model_19 0.685421713191415
+# 2           1         5        0.1             0.2  gbm_grid1_model_9 0.684999851240248
+# 3         0.8         3        0.1             0.5 gbm_grid1_model_18 0.684996004193172
+# 4           1         5        0.1             0.5 gbm_grid1_model_21  0.68475480088044
+# 5           1         3        0.1               1 gbm_grid1_model_31 0.684695040872968
+# 
+# ---
+#   sample_rate max_depth learn_rate col_sample_rate          model_ids               auc
+# 31           1         5       0.01               1 gbm_grid1_model_27 0.668421770188742
+# 32           1         3       0.01             0.2  gbm_grid1_model_1 0.668216786878714
+# 33           1         3       0.01             0.5 gbm_grid1_model_13 0.665042449930355
+# 34         0.8         3       0.01             0.5 gbm_grid1_model_12 0.664959640334883
+# 35         0.8         3       0.01               1 gbm_grid1_model_24 0.662830457801871
+# 36           1         3       0.01               1 gbm_grid1_model_25 0.661749203263473
 
 
 
@@ -85,7 +132,7 @@ print(gbm_gridperf1)
 gbm_params2 <- list(learn_rate = seq(0.01, 0.1, 0.01),
                     max_depth = seq(2, 10, 1),
                     sample_rate = seq(0.5, 1.0, 0.1),
-                    col_sample_rate = c(0.1, 1.0, 0.1))
+                    col_sample_rate = seq(0.1, 1.0, 0.1))
 search_criteria2 <- list(strategy = "RandomDiscrete", 
                          max_models = 36)
 
@@ -103,6 +150,35 @@ gbm_gridperf2 <- h2o.getGrid(grid_id = "gbm_grid2",
                              sort_by = "auc", 
                              decreasing = TRUE)
 print(gbm_gridperf2)
+# H2O Grid Details
+# ================
+#   
+# Grid ID: gbm_grid2 
+# Used hyper parameters: 
+# -  sample_rate 
+# -  max_depth 
+# -  learn_rate 
+# -  col_sample_rate 
+# Number of models: 36 
+# Number of failed models: 0 
+# 
+# Hyper-Parameter Search Summary: ordered by decreasing auc
+# sample_rate max_depth learn_rate col_sample_rate          model_ids               auc
+# 1         0.9         3       0.09             0.7 gbm_grid2_model_12 0.686216400866229
+# 2           1         4       0.07             0.9  gbm_grid2_model_5 0.685706972276958
+# 3         0.7         5       0.08             0.6  gbm_grid2_model_2 0.684295182287367
+# 4         0.5         7       0.06             0.4 gbm_grid2_model_23 0.683939570192207
+# 5         0.8         7       0.04             0.4 gbm_grid2_model_16 0.683870143525371
+# 
+# ---
+#   sample_rate max_depth learn_rate col_sample_rate          model_ids               auc
+# 31         0.8        10       0.06             0.5 gbm_grid2_model_19 0.674279253550808
+# 32         0.8         9       0.09             0.9 gbm_grid2_model_26 0.673252315393779
+# 33         0.5         3       0.02             0.3 gbm_grid2_model_10 0.672512930382942
+# 34         0.5         9       0.09             0.3 gbm_grid2_model_11 0.672470062508521
+# 35         0.7         5       0.01             0.8 gbm_grid2_model_31  0.67178319023517
+# 36           1        10        0.1             0.8  gbm_grid2_model_9  0.66928075695065
+
 
 
 # Looks like learn_rate = 0.1 does well here, which was the biggest 
@@ -115,7 +191,7 @@ print(gbm_gridperf2)
 gbm_params <- list(learn_rate = seq(0.1, 0.3, 0.01),  #updated
                    max_depth = seq(2, 10, 1),
                    sample_rate = seq(0.9, 1.0, 0.05),  #updated
-                   col_sample_rate = c(0.1, 1.0, 0.1))
+                   col_sample_rate = seq(0.1, 1.0, 0.1))
 search_criteria <- list(strategy = "RandomDiscrete", 
                          max_runtime_secs = 60)  #updated
 
@@ -133,6 +209,35 @@ gbm_gridperf <- h2o.getGrid(grid_id = "gbm_grid2",
                              sort_by = "auc", 
                              decreasing = TRUE)
 print(gbm_gridperf)
+# H2O Grid Details
+# ================
+#   
+# Grid ID: gbm_grid2 
+# Used hyper parameters: 
+# -  sample_rate 
+# -  max_depth 
+# -  learn_rate 
+# -  col_sample_rate 
+# Number of models: 72 
+# Number of failed models: 0 
+# 
+# Hyper-Parameter Search Summary: ordered by decreasing auc
+# sample_rate max_depth learn_rate col_sample_rate          model_ids               auc
+# 1           1         4        0.1             0.4 gbm_grid2_model_47   0.6866268688012
+# 2         0.9         3       0.09             0.7 gbm_grid2_model_12 0.686216400866229
+# 3         0.9         3       0.23             0.2 gbm_grid2_model_49 0.685769102632132
+# 4           1         4       0.07             0.9  gbm_grid2_model_5 0.685706972276958
+# 5        0.95         4       0.12             0.3 gbm_grid2_model_69 0.685149079613059
+# 
+# ---
+#   sample_rate max_depth learn_rate col_sample_rate          model_ids               auc
+# 67           1         9       0.22             0.8 gbm_grid2_model_55 0.651326010648147
+# 68           1        10       0.23             0.2 gbm_grid2_model_68 0.651131266150378
+# 69           1         9       0.23             0.4 gbm_grid2_model_64 0.649976105805347
+# 70         0.9        10        0.2             0.4 gbm_grid2_model_59 0.649033295919983
+# 71        0.95        10       0.21             0.5 gbm_grid2_model_65 0.648789760403075
+# 72        0.95         9       0.24             0.3 gbm_grid2_model_63 0.648310176398551
+
 
 # Grab the model_id for the top GBM model, chosen by validation AUC
 best_gbm_model_id <- gbm_gridperf@model_ids[[1]]
@@ -142,7 +247,7 @@ best_gbm <- h2o.getModel(best_gbm_model_id)
 # so we get an honest estimate of top model performance
 best_gbm_perf <- h2o.performance(model = best_gbm, 
                                  newdata = test)
-h2o.auc(best_gbm_perf)  # 0.6843054
+h2o.auc(best_gbm_perf)  # 0.6840662
 
 # As we can see, this is slighly less than the AUC on the validation set
 # of the top model, but this is a more honest estimate of performance.  
@@ -180,6 +285,36 @@ dl_gridperf <- h2o.getGrid(grid_id = "dl_grid",
                            sort_by = "auc", 
                            decreasing = TRUE)
 print(dl_gridperf)
+# H2O Grid Details
+# ================
+#   
+# Grid ID: dl_grid 
+# Used hyper parameters: 
+# -  l2 
+# -  l1 
+# -  activation 
+# Number of models: 53 
+# Number of failed models: 0 
+# 
+# Hyper-Parameter Search Summary: ordered by decreasing auc
+# l2    l1 activation        model_ids               auc
+# 1 1e-04     0     Maxout dl_grid_model_21 0.682435320295004
+# 2 0.001 1e-05  Rectifier dl_grid_model_48 0.681218073187401
+# 3     0 1e-04     Maxout dl_grid_model_17 0.679177558005678
+# 4 1e-05 1e-04  Rectifier dl_grid_model_31 0.677314159563525
+# 5 0.001 0.001  Rectifier dl_grid_model_43 0.676308690762695
+# 
+# ---
+#   l2    l1           activation        model_ids               auc
+# 48     0   0.1 RectifierWithDropout dl_grid_model_22 0.499974988744935
+# 49  0.01   0.1    MaxoutWithDropout dl_grid_model_23  0.49994997748987
+# 50     0   0.1            Rectifier  dl_grid_model_3 0.499835546911143
+# 51     0   0.1    MaxoutWithDropout  dl_grid_model_2 0.499433764802004
+# 52 1e-05   0.1               Maxout dl_grid_model_38 0.499116982816559
+# 53   0.1 0.001    MaxoutWithDropout dl_grid_model_34 0.497202019776002
+
+# Note that that these results are not reproducible since we are not using a single core H2O cluster
+# H2O's DL requires a single core to be used in order to get reproducible results
 
 # Grab the model_id for the top DL model, chosen by validation AUC
 best_dl_model_id <- dl_gridperf@model_ids[[1]]
@@ -189,7 +324,7 @@ best_dl <- h2o.getModel(best_dl_model_id)
 # so we get an honest estimate of top model performance
 best_dl_perf <- h2o.performance(model = best_dl, 
                                 newdata = test)
-h2o.auc(best_dl_perf)  # 0.6798002
+h2o.auc(best_dl_perf)  # 0.6778661 
 
 
 
