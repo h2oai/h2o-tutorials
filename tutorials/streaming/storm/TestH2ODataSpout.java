c5bf5d4f6211076b0323719d64950361b3b13887
Mark Chan <mkchan@gmail.com>
2016-06-07 10:29:50 -0700

    updates the import statements

--- tutorials/streaming/storm/TestH2ODataSpout.java
+++ tutorials/streaming/storm/TestH2ODataSpout.java
@@ -1,13 +1,13 @@
-package storm.starter;
+package org.apache.storm.starter;
 
-import backtype.storm.Config;
-import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseRichSpout;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Values;
-import backtype.storm.utils.Utils;
+import org.apache.storm.Config;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
