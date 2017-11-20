b5e6da85b2d5b10e898e7aa8df6948fa70024511
Tom Kraljevic <tomk@tomk.net>
2017-11-20 07:57:53 -0800

    Switch to a python 3.5 environment.

--- h2o-world-2017/run.sh
+++ h2o-world-2017/run.sh
@@ -21,8 +21,10 @@ echo "-------------------------"
 echo "Welcome to H2O World 2017"
 echo "-------------------------"
 echo ""
-echo "- Connect to Jupyter notebook on port 8888 inside the container (password: h2o)"
-echo "- Connect to RStudio on port 8787 inside the container (username/password: h2o/h2o)"
+echo "- Connect to Jupyter notebook on port 8888 (password: h2o)"
+echo "- Connect to RStudio on port 8787 (username/password: h2o/h2o)"
+
+source /home/h2o/Miniconda3/bin/activate h2o
 
 (cd /home/h2o && \
  jupyter --paths >> "$logdir"/jupyter.log && \
