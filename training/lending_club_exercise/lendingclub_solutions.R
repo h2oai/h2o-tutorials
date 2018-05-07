bf458e078903fd4d9c731eb002477099217f03f3
Patrick Aboyoun <aboyoun@gmail.com>
2018-05-07 15:19:29 -0700

    Update lending club R scripts

--- training/lending_club_exercise/lendingclub_solutions.R
+++ training/lending_club_exercise/lendingclub_solutions.R
@@ -1,12 +1,12 @@
+# Load package and connect to cluster
 library(h2o)
-h2o.init(nthreads = -1)
+h2o.init()
 
 ## If possible download from the s3 link and change the path to the dataset.
-small_test <- "http://h2o-public-test-data.s3.amazonaws.com/bigdata/laptop/lending-club/LoanStats3a.csv" 
-
+small_test <- "http://h2o-public-test-data.s3.amazonaws.com/bigdata/laptop/lending-club/LoanStats3a.csv"
 
 ## Task 1: Import Data
-loan_stats <- h2o.importFile(path = small_test, parse = F)
+loan_stats <- h2o.importFile(path = small_test, parse = FALSE)
 
 ## Specify some column types to "String" that we want to munge later
 parseSetup <- h2o.parseSetup(loan_stats)
@@ -47,7 +47,7 @@ loan_stats$revol_util <- as.numeric(loan_stats$revol_util)
 loan_stats$emp_length <- h2o.sub(x = loan_stats$emp_length, pattern = "([ ]*+[a-zA-Z].*)|(n/a)", replacement = "")
 ## Use h2o.trim to remove any trailing spaces
 loan_stats$emp_length <- h2o.trim(loan_stats$emp_length)
-## Use h2o.sub to convert < 1 to 0 years and do the same for 10 + to 10 
+## Use h2o.sub to convert < 1 to 0 years and do the same for 10 + to 10, then convert to numeric
 ## Hint: Be mindful of spaces between characters
 loan_stats$emp_length <- h2o.sub(x = loan_stats$emp_length, pattern = "< 1", replacement = "0")
 loan_stats$emp_length <- h2o.sub(x = loan_stats$emp_length, pattern = "10\\\\+", replacement = "10")
@@ -63,70 +63,98 @@ loan_stats$verification_status <- h2o.sub(x = loan_stats$verification_status, pa
 loan_stats$verification_status <- h2o.sub(x = loan_stats$verification_status, pattern = "VERIFIED - income", replacement = "verified")
 loan_stats$verification_status <- as.h2o(as.matrix(loan_stats$verification_status))
 
-## Task 8: Do a test-train split (80-20)
-## Hint: Use h2o.splitFrame ONLY once
-split <- h2o.splitFrame(loan_stats, ratios = 0.8)
-train <- split[[1]]
-valid <- split[[2]]
+## Task 8: Create Cross-Validation 5-Fold Column Called "fold"
+## Hint: Use h2o.kfold_column
+cv_nfolds <- 5
+cv_seed <- 54321
+loan_stats$fold <- h2o.kfold_column(loan_stats, nfolds = cv_nfolds, seed = cv_seed)
 
-## Task 9: Define your response and predictor variables
+## Task 9: Create Cross-Validation Target Encoding for Categorial Variables
+##         Home Ownership, Loan Purpose, and State of Residence
+## Hint: use h2o.target_encode_create to create a target encoding mapping and
+##      target_encode_apply to add the new columns to loan_stats
+mapping <- h2o.target_encode_create(data = loan_stats,
+                                    x = list("home_ownership", "purpose", "addr_state"),
+                                    y = "bad_loan",
+                                    fold_column = "fold")
+
+loan_stats <- h2o.target_encode_apply(data = loan_stats,
+                                      x = list("home_ownership", "purpose", "addr_state"),
+                                      y = "bad_loan",
+                                      target_encode_map = mapping,
+                                      holdout_type = "KFold",
+                                      fold_column = "fold")
+
+## Task 10: Define your response and predictor variables
 myY <- "bad_loan"
-myX <-  c("loan_amnt", "term", "home_ownership", "annual_inc", "verification_status", "purpose",
-          "addr_state", "dti", "delinq_2yrs", "open_acc", "pub_rec", "revol_bal", "total_acc",
-          "emp_length", "credit_length", "inq_last_6mths", "revol_util")
+myX <-  c("loan_amnt", "term", "TargetEncode_home_ownership", "annual_inc", "verification_status",
+          "TargetEncode_purpose", "TargetEncode_addr_state", "dti", "delinq_2yrs", "open_acc",
+          "pub_rec", "revol_bal", "total_acc", "emp_length", "credit_length", "inq_last_6mths",
+          "revol_util")
 
-## Task 10: Build model predicting good/bad loan 
-## Note: Use any of the classification methods available including GLM, GBM, Random Forest, and Deep Learning
-gbm_model <- h2o.gbm(x = myX, y = myY, training_frame = train, validation_frame = valid,
-                     learn_rate = 0.05, score_each_iteration = T, ntrees = 100, model_id = "GBM_BadLoan")
+## Task 11: Use Auto ML to build predictive models for good/bad loan
+## Note: Limit max_runtime_secs to something reasonable, e.g. 300 seconds
+aml <- h2o.automl(x = myX, y = myY, training_frame = loan_stats, nfolds = cv_nfolds, fold_column = "fold",
+                  max_runtime_secs = 300, seed = 534, project_name = "AutoML_BadLoan")
 
-## Task 11: Plot the scoring history to make sure you're not overfitting
+## Task 12: Select the best single model
+best_model <- h2o.getModel(aml@leaderboard[3, "model_id"])
+
+## Task 13: Plot the scoring history to make sure you're not overfitting
 ## Hint: Use plot function on the model object
-plot(gbm_model)
+plot(best_model)
 
-## Task 12: Plot the ROC curve for the binomial models and get auc using h2o.auc
+## Task 14: Plot the ROC curve for the binomial models and get auc using h2o.auc
 ## Hint: Use h2o.performance and plot to grab the modelmetrics and then plotting the modelmetrics
-perf <- h2o.performance(model = gbm_model)
-plot(perf, train = T)
-plot(perf, valid = T)
-h2o.auc(gbm_model, train = T)
-h2o.auc(gbm_model, valid = T)
+perf_train <- h2o.performance(model = best_model, train = TRUE)
+perf_valid <- h2o.performance(model = best_model, valid = TRUE)
+perf_xval <- h2o.performance(model = best_model, xval = TRUE)
+plot(perf_train, col = "blue")
+plot(perf_valid, col = "orange")
+plot(perf_xval, col = "brown")
+h2o.auc(best_model, train = TRUE)
+h2o.auc(best_model, valid = TRUE)
+h2o.auc(best_model, xval = TRUE)
 
-## Task 13: Check the variable importance and generate confusion matrix for max F1 threshold
-## Hint: Use h2o.varimp for non-GLM model and use h2o.confusionMatrix
-h2o.varimp(gbm_model)
-h2o.confusionMatrix(gbm_model, valid = TRUE)
+## Task 15: Check the variable importance and generate confusion matrix for max F1 threshold
+## Hint: Use h2o.varimp_plot, h2o.varimp, and h2o.confusionMatrix
+h2o.varimp_plot(best_model)
+h2o.varimp(best_model)
+h2o.confusionMatrix(best_model, valid = TRUE)
+h2o.confusionMatrix(best_model, xval = TRUE)
 
-## Task 14: Score the entire data set using the model
+## Task 16: Score the entire data set using the model
 ## Hint: Use h2o.predict.
-pred <- h2o.predict(gbm_model, loan_stats)
+pred <- h2o.predict(best_model, loan_stats)
 
 ## Extra: Calculate the money gain/loss if model is implemented
 ## Calculate the total amount of money earned or lost per loan
 loan_stats$earned <- loan_stats$total_pymnt - loan_stats$loan_amnt
 
 ## Calculate how much money will be lost to false negative, vs how much will be saved due to true positives
-loan_stats$pred <- pred[,1]
-net <- as.data.frame(h2o.group_by(data = loan_stats, by = c("bad_loan", "pred"), gb.control = list(na.methods = "ignore"), sum("earned")))
-n1  <- net[ net$bad_loan == 0 & net$pred == 0, 3]
-n2  <- net[ net$bad_loan == 0 & net$pred == 1, 3]
-n3  <- net[ net$bad_loan == 1 & net$pred == 1, 3]
-n4  <- net[ net$bad_loan == 1 & net$pred == 0, 3]
-
+loan_stats$pred <- pred[, 1]
+net <- as.data.frame(h2o.group_by(data = loan_stats,
+                                  by = c("bad_loan", "pred"),
+                                  gb.control = list(na.methods = "ignore"),
+                                  sum("earned")))
+n1 <- net[net$bad_loan == 0 & net$pred == 0, 3]
+n2 <- net[net$bad_loan == 0 & net$pred == 1, 3]
+n3 <- net[net$bad_loan == 1 & net$pred == 1, 3]
+n4 <- net[net$bad_loan == 1 & net$pred == 0, 3]
 
 ## Function defined to pretty print numerics as dollars
 printMoney <- function(x){
-  x <- round(abs(x),2)
-  format(x, digits=10, nsmall=2, decimal.mark=".", big.mark=",")
+  x <- round(abs(x), 2)
+  format(x, digits = 10, nsmall = 2, decimal.mark = ".", big.mark = ",")
 }
 
 ## Calculate the amount earned
-print(paste0("Total amount of profit still earned using the model : $", printMoney(n1) , ""))
-print(paste0("Total amount of profit forfeitted using the model : $", printMoney(n2) , ""))
-print(paste0("Total amount of loss that could have been prevented : $", printMoney(n3) , ""))
-print(paste0("Total amount of loss that still would've accrued : $", printMoney(n4) , ""))
+writeLines(sprintf("Total amount of profit still earned using the model : $%s", printMoney(n1)))
+writeLines(sprintf("Total amount of profit forfeitted using the model : $%s", printMoney(n2)))
+writeLines(sprintf("Total amount of loss that could have been prevented : $%s", printMoney(n3)))
+writeLines(sprintf("Total amount of loss that still would've accrued : $%s", printMoney(n4)))
 ## Calculate Net
-print(paste0("Total profit by implementing model : $", printMoney( n1 - n2 + abs(n3) - abs(n4))))
+writeLines(sprintf("Total profit by implementing model : $%s", printMoney( n1 - n2 + abs(n3) - abs(n4))))
 
 # Shutdown h2o instance
 h2o.shutdown()
