26c1ca69710a45685cabac2ac80a6510710fb211
Tom Kraljevic <tomk@tomk.net>
2018-06-25 18:16:52 -0700

    More tweaks for nyc workshop.

--- nyc-workshop-2018/h2o_sw/run.sh
+++ nyc-workshop-2018/h2o_sw/run.sh
@@ -17,21 +17,17 @@ fi
 logdir=/log/`date "+%Y%m%d-%H%M%S"`
 mkdir -p "$logdir"
 
-echo "-------------------------"
-echo "Welcome to H2O World 2017"
-echo "-------------------------"
+echo "----------------------------"
+echo "Welcome to NYC Workshop 2018"
+echo "----------------------------"
 echo ""
 echo "- Connect to Jupyter notebook on port 8888 (password: h2o)"
-echo "- Connect to RStudio on port 8787 (username/password: h2o/h2o)"
 
 source /home/h2o/Miniconda3/bin/activate h2o
 
 (cd /home/h2o && \
  jupyter --paths >> "$logdir"/jupyter.log && \
  nohup jupyter notebook --ip='*' --no-browser --allow-root >> "$logdir"/jupyter.log 2>&1 &)
-
-(cd /home/h2o && \
- sudo rstudio-server start >> "$logdir"/rstudio-server.log)
 
 # 10 years
 sleep 3650d
