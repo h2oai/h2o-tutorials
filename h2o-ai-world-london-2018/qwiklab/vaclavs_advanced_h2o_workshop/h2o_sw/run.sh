3a380fa7c59841601387f61bf07f1a4ce9668e12
Vaclav Belak <vaclav@h2o.ai>
2018-10-25 16:41:05 +0200

    RStudio and R example added

--- h2o-ai-world-london-2018/qwiklab/vaclavs_advanced_h2o_workshop/h2o_sw/run.sh
+++ h2o-ai-world-london-2018/qwiklab/vaclavs_advanced_h2o_workshop/h2o_sw/run.sh
@@ -22,12 +22,16 @@ echo "Welcome to H2O World 2018 London Workshop"
 echo "----------------------------"
 echo ""
 echo "- Connect to Jupyter notebook on port 8888 (password: h2o)"
+echo "- Connect to RStudio on port 8787 (user/pass: h2o)"
 
 source /home/h2o/Miniconda3/bin/activate h2o
 
 (cd /home/h2o && \
  jupyter --paths >> "$logdir"/jupyter.log && \
  nohup jupyter notebook --ip='*' --no-browser --allow-root >> "$logdir"/jupyter.log 2>&1 &)
+
+(cd /home/h2o && \
+ sudo rstudio-server start >> "$logdir"/rstudio-server.log)
 
 # 10 years
 sleep 3650d
