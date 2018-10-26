37492e6112af9aa9689597e2a31a7823f259b416
Tom Kraljevic <tomk@tomk.net>
2018-10-26 03:10:28 -0700

    Move Vaclav's h2o ai world london 2018 to inside training directory.

--- training/run.sh
+++ training/run.sh
@@ -17,11 +17,12 @@ fi
 logdir=/log/`date "+%Y%m%d-%H%M%S"`
 mkdir -p "$logdir"
 
-echo "----------------------------"
-echo "Welcome to NYC Workshop 2018"
-echo "----------------------------"
+echo "--------------------------"
+echo "Welcome to H2O.ai Training"
+echo "--------------------------"
 echo ""
 echo "- Connect to Jupyter notebook on port 8888 (password: h2o)"
+echo "- Connect to RStudio server on port 8787 (user: h2o / password: h2o)"
 
 source /home/h2o/Miniconda3/bin/activate h2o
 
@@ -29,5 +30,7 @@ source /home/h2o/Miniconda3/bin/activate h2o
  jupyter --paths >> "$logdir"/jupyter.log && \
  nohup jupyter notebook --ip='*' --no-browser --allow-root >> "$logdir"/jupyter.log 2>&1 &)
 
-# 10 years
-sleep 3650d
+(cd /home/h2o && \
+ sudo rstudio-server start >> "$logdir"/rstudio-server.log)
+
+sleep infinity
