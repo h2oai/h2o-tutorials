3fd62dbee261fb432e4584948024c4a080573675
Jakub Hava <jakub.hava@gmail.com>
2019-02-01 18:02:11 -0800

    Fix start streaming

--- training/sparkling_water_hands_on/sentiment_analysis/start_streaming.sh
+++ training/sparkling_water_hands_on/sentiment_analysis/start_streaming.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-nohup $(pwd)/start_streaming2.sh & disown
+DATASET="../../data/topics/sentiment_analysis/AmazonReviews_Train.csv"
+OUTPUT=$(pwd)/"output"
+mkdir -p $OUTPUT
+
+i=0
+while read l; do
+  echo "$l" > $OUTPUT/${i}.csv
+  i=$((i+1))
+  sleep 1
+done <$DATASET
+
